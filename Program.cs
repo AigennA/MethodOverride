0dad3d006a8e62291c64cbbac99caeca8f4385ff
Aigen80 <aomurm@gmail.com>
2025-09-16 11:35:36 +0200

    Skapat project med 3 method med olika parametrar

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace MethodOverride
+﻿
+using System;
+
+class Program
 {
-    internal class Program
+    //Metod 1: Format namn med för- och efternamn
+    static string FormatName(string first, string last)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        return $"{first} {last}";
+    }
+
+    //Metod 2: Format namn med för-, mellan- och efternamn
+    static string FormatName(string first, string middle, string last)
+    {
+        return $"{first} {middle} {last}";
+    }
+
+    //Metod 3: Formatnamn med möjlighet att göra det versalt
+    static string FormatName(string first, string last, bool uppercase)
+    {
+        var full = $"{first} {last}";
+        return uppercase ? full.ToUpper() : full;
+    }
+
+    static void Main(string[] args)
+    {
+        // Anrop alla tre metoder
+        Console.WriteLine(FormatName("Maria", "Johansson"));
+        Console.WriteLine(FormatName("Maria", "Eva", "Johansson"));
+        Console.WriteLine(FormatName("Maria", "Johansson", true));
     }
 }
